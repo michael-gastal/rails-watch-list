e5ecad23def1098264af53f862340e7a9cfba1a8
michael gastal <michaelgastal@gmail.com>
2021-05-29 10:28:03 +0200

    fix bug with bookmarks/:id path 5

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[new create]
   end
-  resources :bookmarks, only: :destroy
+  resources :bookmarks, only: [:destroy]
 end
